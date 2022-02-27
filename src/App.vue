1d972fda03830de4687345376f98bb448ed655aa
Dustella <fdnoaivj@outlook.com>
2022-02-27 19:59:44 +0800

    播放音乐完成

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,16 @@
     <!-- todo: There should be a better logo-->
     <div class="row">
       <el-input v-model="keyword" placeholder="输入搜索的音乐" />
-      <dialog-search :keyword="keyword" />
+      <dialog-search :keyword="keyword" @play="play" />
       <dialog-rank />
     </div>
-    <vue3-video-play v-bind="options" />
+    <vue3-video-play
+      v-bind="options"
+      @play="isplaying"
+      @pause="pause"
+      @timeupdate="updateDuration"
+    />
+    <audio :src="src" loop id="au"></audio>
   </main-container>
 </template>
 
@@ -16,30 +22,42 @@ import { ref, reactive } from "vue";
 import MainContainer from "./components/container.vue";
 import dialogSearch from "./components/dialogSearch.vue";
 import DialogRank from "./components/dialogRank.vue";
+import getUrl from "./apis/getUrl";
+import { ElMessage } from "element-plus";
 
-const keyword = ref("海阔天空");
+const keyword = ref("");
+const src = ref("");
+
+const play = (id: number) => {
+  getUrl(id).then((url) => {
+    src.value = url;
+    ElMessage({
+      message: "音乐已经成功装载！",
+      type: "success",
+    });
+  });
+};
+
 const options = reactive({
-  width: "100%", //播放器高度
-  height: "65vh", //播放器高度
-  src: "https://s2-cdn.dustella.net/ide_full_min.mp4", //视频源
+  width: "100%",
+  height: "65vh",
+  src: "https://s2-cdn.dustella.net/ide_full_min.mp4",
   muted: false,
-  speedRate: ["0.75", "1.0", "1.25", "1.5", "2.0"], //播放倍速
-  autoPlay: false, //自动播放
-  loop: false, //循环播放
-  mirror: false, //镜像画面
-  ligthOff: false, //关灯模式
-  volume: 0.3, //默认音量大小
-  control: true, //是否显示控制
-  controlBtns: [
-    "audioTrack",
-    "quality",
-    "speedRate",
-    "volume",
-    "setting",
-    "pip",
-    "fullScreen",
-  ], //显示所有按钮,
 });
+
+const auPlayer = (): HTMLAudioElement => {
+  return document.getElementById("au") as HTMLAudioElement;
+};
+
+const isplaying = () => {
+  auPlayer().play();
+};
+
+const pause = () => {
+  auPlayer().pause();
+};
+
+const updateDuration = () => {};
 </script>
 
 <style lang="scss">
@@ -48,7 +66,7 @@ const options = reactive({
   flex-direction: row;
   justify-content: center;
   & > * {
-    margin: 0 10px 0 10px;
+    margin-right: 10px;
   }
 }
 :deep(.el-input__inner) {
--- src/components/dialogSearch.vue
+++ src/components/dialogSearch.vue
@@ -1,38 +1,64 @@
 <template>
-  <el-button type="primary" @click="openSearch = true">开始</el-button>
+  <el-button type="primary" @click="search">开始</el-button>
   <el-dialog v-model="openSearch">
     <el-card v-for="i of res" :key="i.Name" class="m-card">
       <el-image :src="i.alSrc" alt="专辑图" />
-      <el-descriptions :title="i.Name" border>
+      <el-descriptions :title="i.Name" style="width: 600px">
         <el-descriptions-item label="专辑">{{ i.alName }}</el-descriptions-item>
         <el-descriptions-item label="歌手">{{ i.arName }}</el-descriptions-item>
       </el-descriptions>
+      <div style="width: 100%">
+        <el-button type="primary" @click="play(i.Id)">播放</el-button>
+      </div>
     </el-card>
   </el-dialog>
 </template>
 
 <script lang="ts" setup>
-import { ref, defineProps, watchEffect } from "vue";
+import { ref, defineProps, defineEmits, watchEffect } from "vue";
 import querySearch from "../apis/querySearch";
 import { Music } from "../apis/MusicBuilder";
+import { ElMessage } from "element-plus";
 
 const props = defineProps({
   keyword: String,
 });
 
+const emit = defineEmits(["play"]);
+
 let res = ref([] as Music[]);
-const openSearch = ref(true);
+const openSearch = ref(false);
+const search = () => {
+  if (props.keyword != "") {
+    openSearch.value = true;
+  } else {
+    ElMessage({
+      message: "请输入需要搜索的音乐",
+      type: "warning",
+    });
+  }
+};
 watchEffect(() => {
   querySearch(props.keyword).then((result) => (res.value = result));
 });
+
+const play = (id: number) => {
+  emit("play", id);
+  openSearch.value = false;
+};
 </script>
 
 <style scoped lang="scss">
 .m-card {
   height: 10vh;
+  margin-top: 5px;
   & .el-image {
     height: 100%;
-    width: 6vh;
+    width: 10%;
+    margin-right: 15px;
+  }
+  & .el-button {
+    float: right;
   }
 }
 :deep(.el-card__body) {
